c14949b9cbbe3aa9d7221592640036eecc257240
陆 磊 <446194074@qq.com>
2017-02-17 17:49:09 +0800

    add files

---
+++ app/src/main/java/me/looorielovbb/boom/network/api/ZhihuApi.java
@@ -0,0 +1,12 @@
+package me.looorielovbb.boom.network.api;
+
+/**
+ * Created by Lulei on 2017/2/17.
+ * time : 15:58
+ * date : 2017/2/17
+ * mail to lulei4461@gmail.com
+ */
+
+public interface ZhihuApi {
+    public final String ZHIHU = "";
+}
